abeaf83c9316eb1ac1238d940b9a4f7d09d87f1d
Edward Apostol <edward.apostol.github@gmail.com>
2017-02-07 12:49:18 -0500

    #005
    implemented ES6 solution

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
  * Created by Edward_J_Apostol on 2017-02-07.
  */
 
+import VideoPlayer from './VideoPlayer';
+
 export default class App{
 
     constructor(){
         console.log("instance of App is created");
+        this.videoPlayer = new VideoPlayer();
     }
-
 }
---
+++ src/VideoPlayer.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Edward_J_Apostol on 2017-02-07.
+ */
+
+export default class VideoPlayer{
+
+    constructor(){
+        this.player = document.getElementById("video_player");
+        this.links = this.player.getElementsByTagName('a');
+        this.videotarget = "";
+        this.fileName = "";
+        this.video = null;
+        this.source = "";
+        this.initLinks(this.links);
+    }
+
+    initLinks(links){
+        for (let i=0; i<links.length; i++) {
+            // this.links[i].onclick = handler;
+            (links[i]).addEventListener("click",this.linkHandler,false)
+        }
+    }
+
+    linkHandler(e){
+        // e.stopPropagation();
+        e.preventDefault();
+        this.videotarget = this.getAttribute("href");
+        this.fileName = this.videotarget.substr(0, this.videotarget.lastIndexOf('.')) || this.videotarget;
+        this.video = document.querySelector("#video_player video");
+        this.video.removeAttribute("controls");
+        this.video.removeAttribute("poster");
+
+        this.source = document.querySelectorAll("#video_player video source");
+        this.source[0].src = this.fileName + ".mp4";
+        this.source[1].src = this.fileName + ".webm";
+        this.video.load();
+        this.video.play();
+    }
+}
