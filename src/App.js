60b569a20c760e998c72058f932f7b014fc635f6
Edward Apostol <edward.apostol.github@gmail.com>
2017-02-07 11:51:15 -0500

    #005: create sample ES6 files
    create sample ES6 files for transpiling.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Edward_J_Apostol on 2017-02-07.
+ */
+
+export default class App{
+
+    constructor(){
+        console.log("instance of App is created");
+    }
+
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Edward_J_Apostol on 2017-02-07.
+ */
+
+import App from './App';
+
+let app = new App();
